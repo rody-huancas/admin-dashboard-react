7aa0e7c1d345c542b6e82531500fba2929ebad03
Rody Huancas <rodyhuancas.04@gmail.com>
2024-08-21 21:09:20 -0500

    Feat: sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
-import { Route, Routes } from "react-router-dom"
-import { OverviewPage, ProductsPage } from "./pages"
+import { Route, Routes } from "react-router-dom";
+import { OverviewPage, ProductsPage } from "./pages";
+import { Sidebar } from "./components";
 
 const App = () => {
   return (
     <div className="flex h-screen bg-gray-900 text-gray-100 overflow-hidden">
+      {/* BG */}
+      <div className="fixed inset-0 z-0">
+        <div className="absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80" />
+        <div className="absolute inset-0 backdrop-blur-sm" />
+      </div>
+
+      <Sidebar />
+      
       <Routes>
         <Route path="/" element={<OverviewPage />} />
         <Route path="/products" element={<ProductsPage />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
