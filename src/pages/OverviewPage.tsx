bb827a09536785f6bb24f6a6d1450e67bc75d99f
Rody Huancas <rodyhuancas.04@gmail.com>
2024-08-22 20:20:10 -0500

    Feat: product page

--- src/pages/OverviewPage.tsx
+++ src/pages/OverviewPage.tsx
@@ -8,7 +8,7 @@ export const OverviewPage = () => {
     <div className="flex-1 overflow-auto relative z-10">
       <Header title="Overview" />
 
-      <main className="max-w-7xl mx-auto py-6 px-4 lg:px-8">
+      <main className="max-w-7xl 2xl:max-w-[80%] mx-auto py-6 px-4 lg:px-8">
         <motion.div
           className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4 mb-8"
           initial={{ opacity: 0, y: 20 }}
--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -1,5 +1,26 @@
+import { motion } from 'framer-motion';
+import { BarChart2, ShoppingBag, Users, Zap } from "lucide-react";
+
+import { Header, StatCard } from "../components";
+
 export const ProductsPage = () => {
   return (
-    <div>ProductsPage</div>
-  )
-}
+    <div className="flex-1 overflow-auto relative z-10">
+      <Header title="Products" />
+
+      <main className="max-w-7xl 2xl:max-w-[80%] mx-auto py-6 px-4 lg:px-8">
+        <motion.div
+          className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4 mb-8"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+        >
+          <StatCard name="Total Sales" icon={Zap} value="$12,345" color="#6366F1" />
+          <StatCard name="New Users" icon={Users} value="1,234" color="#8B5CF6" />
+          <StatCard name="Total Products" icon={ShoppingBag} value="567" color="#EC4899" />
+          <StatCard name="Conversion Rate" icon={BarChart2} value="12.5%" color="#10B981" />
+        </motion.div>
+        </main>
+    </div>
+  );
+};
